0018a1fb24a00d465720e8422e2c54d2a6eb3c94
Fancywt <1121545183@qq.com>
2021-06-18 22:17:03 +0800

    Java-Git-Idea-Test

---
+++ src/main/java/com/fancy/git/GitTestJava.java
@@ -0,0 +1,11 @@
+package com.fancy.git;
+
+/**
+ * @Author:Fancy
+ * @version: 1.0
+ */
+public class GitTestJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!!!!");
+    }
+}
