13064ff39ae42f54f8e6d31f7872c54c10f7590c
Fancywt <1121545183@qq.com>
2021-06-18 23:38:26 +0800

    Java-Git-Idea-Test -7 push

--- src/main/java/com/fancy/git/GitTestJava.java
+++ src/main/java/com/fancy/git/GitTestJava.java
@@ -3,7 +3,7 @@ package com.fancy.git;
 
 /**
  * @Author:Fancy
- * @version: 6.0
+ * @version: 7.0
  */
 public class GitTestJava {
     public static void main(String[] args) {
@@ -11,5 +11,6 @@ public class GitTestJava {
         System.out.println("Hello Git!!!!!!,更改version--4 hot-fix");
         System.out.println("Hello Git!!!!!!,更改version--5 hot-fix2");
         System.out.println("Hello Git!!!!!!,更改version--6 master");
+        System.out.println("Hello Git!!!!!!,给改version--7 Push");
     }
 }
