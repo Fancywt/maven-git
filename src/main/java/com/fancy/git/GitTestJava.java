61c3fc5bdcce31123384581fd1b299fb9070bf61
Fancywt <1121545183@qq.com>
2021-06-18 23:02:46 +0800

    Java-Git-Idea-Test -5 hot-fix2

--- src/main/java/com/fancy/git/GitTestJava.java
+++ src/main/java/com/fancy/git/GitTestJava.java
@@ -3,11 +3,12 @@ package com.fancy.git;
 
 /**
  * @Author:Fancy
- * @version: 4.0
+ * @version: 5.0
  */
 public class GitTestJava {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!!!!,更改version--2");
         System.out.println("Hello Git!!!!!!,更改version--4 hot-fix");
+        System.out.println("Hello Git!!!!!!,更改version--5 hot-fix2");
     }
 }
