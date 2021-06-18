0b640404fc06cecd37dfa503f9f7a9eb6eae10c1
Fancywt <1121545183@qq.com>
2021-06-18 22:22:18 +0800

    版本2

--- src/main/java/com/fancy/git/GitTestJava.java
+++ src/main/java/com/fancy/git/GitTestJava.java
@@ -1,11 +1,12 @@
+
 package com.fancy.git;
 
 /**
  * @Author:Fancy
- * @version: 1.0
+ * @version: 2.0
  */
 public class GitTestJava {
     public static void main(String[] args) {
-        System.out.println("Hello Git!!!!!!");
+        System.out.println("Hello Git!!!!!!,更改version--2");
     }
 }
